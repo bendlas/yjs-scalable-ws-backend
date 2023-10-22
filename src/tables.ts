1889d891b76391d5fe708dfcc0607013d3f55e46
Natox Local <herwig@natox.local>
2023-10-22 03:07:33 +0200

    make tables creation optional

--- src/tables.ts
+++ src/tables.ts
@@ -7,15 +7,29 @@ export const create = async () => {
       t.text('docname').index();
       t.binary('update');
     });
+    console.log('table created')
+    return true;
+  } else {
+    return false;
   }
 }
 
 export const drop = async () => {
   if (await knex.schema.hasTable('items')) {
     await knex.schema.dropTable('items');
+    console.log('table dropped')
+    return true;
+  } else {
+    return false;
   }
 }
 
+export const recreate = async () => {
+    await drop();
+    await create()
+}
+
 if (import.meta.url === `file://${process.argv[1]}`) {
-  drop().then(() => create()).then(() => console.log('table created')).then(() => knex.destroy());
+    (process.argv[2] === "--force" ? recreate() : create())
+        .then(() => knex.destroy());
 }
